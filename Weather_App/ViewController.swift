9be8aec1112198d5fdbee6eed0a304cd57d54d2e
JEFF GROSS <jeffgross92@gmail.com>
2014-11-06 11:43:05 -0500

    Initial Commit

---
+++ Weather_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather_App
+//
+//  Created by Jeffrey Gross on 11/6/14.
+//  Copyright (c) 2014 NMIX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
